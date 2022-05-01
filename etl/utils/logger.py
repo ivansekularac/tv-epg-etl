11fe7c4fc2fe8d6f6c0e438e4e8ef46a82240b9f
Ivan Sekularac <ivan.sekularac@hotmail.com>
2022-05-01 19:16:04 +0200

    Fixed logger file path issues. Cleaned requirements.txt file

--- etl/utils/logger.py
+++ etl/utils/logger.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from os import path
 
 
 class Logger:
@@ -7,12 +8,15 @@ class Logger:
     def initialize():
         """Logger setup and initialization"""
         now = datetime.now().strftime("%Y%m%d%H%M%S")
-        file_name = f"logs/{now}.log"
+        logs_dir = path.abspath(path.join(path.dirname(__file__), "..", "..", "logs"))
+        file_name = f"{now}.log"
+        file_path = path.join(logs_dir, file_name)
         logging.basicConfig(
-            filename=file_name,
+            filename=file_path,
             filemode="w",
             format="%(asctime)s - %(levelname)s - %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
             level=logging.INFO,
         )
         logging.getLogger()
+        logging.info(f"Logging initialized to {file_path}")
